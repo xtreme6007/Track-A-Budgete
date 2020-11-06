bd2f06e9b2f6499e059fcc8d6895972d4e504b18
xtreme6007 <prestonnichols96@gmail.com>
2020-11-06 10:45:11 -0600

    fixed caching issue

--- public/db.js
+++ public/db.js
@@ -1,14 +1,14 @@
 let db;
 // create a new db request for a "budget" database.
-const request = indexedDB.open("budget", 1);
+const request = indexedDB.open("budget2", 1);
 
-request.onupgradeneeded = function(event) {
-   // create object store called "pending" and set autoIncrement to true
+request.onupgradeneeded = function (event) {
+  // create object store called "pending" and set autoIncrement to true
   const db = event.target.result;
   db.createObjectStore("pending", { autoIncrement: true });
 };
 
-request.onsuccess = function(event) {
+request.onsuccess = function (event) {
   db = event.target.result;
 
   // check if app is online before reading from db
@@ -16,8 +16,8 @@ request.onsuccess = function(event) {
     checkDatabase();
   }
 };
-//p
-request.onerror = function(event) {
+
+request.onerror = function (event) {
   console.log("Woops! " + event.target.errorCode);
 };
 
@@ -40,9 +40,9 @@ function checkDatabase() {
   // get all records from store and set to a variable
   const getAll = store.getAll();
 
-  getAll.onsuccess = function() {
+  getAll.onsuccess = function () {
     if (getAll.result.length > 0) {
-      fetch("/api/transaction", {
+      fetch("/api/transaction/bulk", {
         method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
@@ -50,17 +50,17 @@ function checkDatabase() {
           "Content-Type": "application/json"
         }
       })
-      .then(response => response.json())
-      .then(() => {
-        // if successful, open a transaction on your pending db
-        const transaction = db.transaction(["pending"], "readwrite");
+        .then(response => response.json())
+        .then(() => {
+          // if successful, open a transaction on your pending db
+          const transaction = db.transaction(["pending"], "readwrite");
 
-        // access your pending object store
-        const store = transaction.objectStore("pending");
+          // access your pending object store
+          const store = transaction.objectStore("pending");
 
-        // clear all items in your store
-        store.clear();
-      });
+          // clear all items in your store
+          store.clear();
+        });
     }
   };
 }
--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
+
+
 
 const app = express();
 
@@ -27,9 +29,7 @@ mongoose.connect(
 
 // routes
 app.use(require("./routes/api.js"));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/index.html"));
-});
+
 
 app.listen(PORT, () => {
   console.log(`App running on port localhost:${PORT}!`);
